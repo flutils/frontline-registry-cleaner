8b35a9fdc04a5c91998a8933a285d7093c152747
Richard Peck <rpeck@frontlineutilities.co.uk>
2017-08-18 08:55:11 +0100

    Main Window

--- 3.0/FLCleaner_NET/MainWindow.xaml.cs
+++ 3.0/FLCleaner_NET/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -15,14 +16,77 @@ using System.Windows.Shapes;
 
 namespace FLCleaner_NET
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    // Interaction logic for MainWindow.xaml
+    // This creates the GUI and should call the Engine to get all required data
     public partial class MainWindow : Window
     {
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+        // Defs //
+        [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,      // x-coordinate of upper-left corner
+            int nTopRect,       // y-coordinate of upper-left corner
+            int nRightRect,     // x-coordinate of lower-right corner
+            int nBottomRect,    // y-coordinate of lower-right corner
+            int nWidthEllipse,  // height of ellipse
+            int nHeightEllipse  // width of ellipse
+        );
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+        // App //
         public MainWindow()
         {
             InitializeComponent();
+
+            // Border Radius 
+            // https://stackoverflow.com/a/18822204/1143732
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
         }
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+    // Extras //
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+        // Hover Cursor
+        public void MainWindow_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.MessageBox.Show("Dot Net Perls is awesome");
+        }
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
+        // Draggable Form
+        // https://stackoverflow.com/a/1592899/1143732
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        // Mousedown Event
+        private void MainWindow_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                //SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+    ///////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
+
     }
 }
