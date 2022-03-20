387ee3ab152eae8b13a427583b8f4f1f46dfc4b4
Richard Peck <rpeck@frontlineutilities.co.uk>
2022-03-20 10:29:04 +0000

    Quick Links Styling & Functionality

--- FrontLine GUI (HD)/Pages/Scan.xaml.cs
+++ FrontLine GUI (HD)/Pages/Scan.xaml.cs
@@ -24,5 +24,40 @@ namespace FrontLineGUI
         {
             InitializeComponent();
         }
+
+        // Quick Links
+        // Click handler for each button
+        private void QuickLinksButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button srcButton = e.Source as Button;
+            string tag = srcButton.Tag.ToString();
+            string page = "";
+            if (!String.IsNullOrEmpty(tag))
+            {
+                // Behaviour/Pages
+                switch (tag)
+                {
+                    case "Clean":
+                        page = "Scan";
+                        break;
+
+                    case "Junk":
+                        page = "Scan";
+                        break;
+
+                    case "Defragment":
+                        page = "Extras";
+                        break;
+
+                    case "Startup":
+                        page = "Extras";
+                        break;
+                }
+
+                // https://stackoverflow.com/a/26969093/1143732
+                if (!String.IsNullOrEmpty(page)) NavigationService.Navigate(new Uri($"Pages/{page}.xaml", UriKind.Relative));
+
+            }
+        }
     }
 }
