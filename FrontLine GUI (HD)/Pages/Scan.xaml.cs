11d3fe6b89999a0eff7582850a0e8207382f3e00
richpeck <rpeck@frontlineutilities.co.uk>
2022-08-15 09:44:53 +0100

    Updated Monitoring Integration

--- FrontLine GUI (HD)/Pages/Scan.xaml.cs
+++ FrontLine GUI (HD)/Pages/Scan.xaml.cs
@@ -1,11 +1,20 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Forms;
 using Microsoft.VisualBasic.Devices;
+using System.Threading;
 
 namespace FrontLineGUI
 {
     //Scan Logic
     public partial class Scan : Page
     {
+
+        // RPECK 15/08/2022
+        // Added for performance monitoring
+        public static CPUIDSDK pSDK;
+
+        private static volatile bool _shouldStop = false;
+
         public Scan()
         {
             // Initialize the form
@@ -13,6 +22,93 @@ namespace FrontLineGUI
 
             // Required for OSVersion
             this.DataContext = this;
+
+            // Thread
+            Thread UpdateThread;
+            bool res;
+            int dll_version = 0;
+            int error_code = 0, extended_error_code = 0;
+            string error_message;
+
+            UpdateThread = new Thread(new ThreadStart(ThreadLoop));
+
+            pSDK = new CPUIDSDK();
+            pSDK.CreateInstance();
+
+            res = pSDK.Init(CPUIDSDK.szDllPath,
+                            CPUIDSDK.szDllFilename,
+                            CPUIDSDK.CPUIDSDK_CONFIG_USE_EVERYTHING,
+                            ref error_code,
+                            ref extended_error_code);
+
+            if (error_code != CPUIDSDK.CPUIDSDK_ERROR_NO_ERROR)
+            {
+                //	Init failed, check errorcode
+                switch ((uint)error_code)
+                {
+                    case CPUIDSDK.CPUIDSDK_ERROR_EVALUATION:
+                        {
+                            switch ((uint)extended_error_code)
+                            {
+                                case CPUIDSDK.CPUIDSDK_EXT_ERROR_EVAL_1:
+                                    error_message = "You are running a trial version of the DLL SDK. In order to make it work, please run CPU-Z at the same time.";
+                                    break;
+
+                                case CPUIDSDK.CPUIDSDK_EXT_ERROR_EVAL_2:
+                                    error_message = "Evaluation version has expired.";
+                                    break;
+
+                                default:
+                                    error_message = "Eval version error " + extended_error_code;
+                                    break;
+                            }
+                        }
+                        break;
+
+                    case CPUIDSDK.CPUIDSDK_ERROR_DRIVER:
+                        error_message = "Driver error " + extended_error_code;
+                        break;
+
+                    case CPUIDSDK.CPUIDSDK_ERROR_VM_RUNNING:
+                        error_message = "Virtual machine detected.";
+                        break;
+
+                    case CPUIDSDK.CPUIDSDK_ERROR_LOCKED:
+                        error_message = "SDK mutex locked.";
+                        break;
+
+                    default:
+                        error_message = "Error code 0x%X" + error_code;
+                        break;
+                }
+                MessageBox.Show(error_message, "CPUID SDK Error");
+            }
+
+            if (res)
+            {
+                pSDK.GetDllVersion(ref dll_version);
+
+                UpdateThread.Start();
+
+                
+
+                _shouldStop = true;
+                UpdateThread.Join();
+            }
+
+            pSDK.Close();
+            pSDK.DestroyInstance();
+
+        }
+
+        // Threadloop
+        public static void ThreadLoop()
+        {
+            while (!_shouldStop)
+            {
+                pSDK.RefreshInformation();
+                Thread.Sleep(1000);
+            }
         }
 
         // OS Version
